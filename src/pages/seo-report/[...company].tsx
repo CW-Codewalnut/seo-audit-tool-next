2d2e35b5b7157c9fc5caa4fc16a84934a1b31a22
Lovepreet <lovepreet@codewalnut.com>
2023-10-04 12:13:52 +0530

    fix: change download button

--- src/pages/seo-report/[...company].tsx
+++ src/pages/seo-report/[...company].tsx
@@ -1,25 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { technicalTerms } from "@/Constants/constants";
-import { toast } from "react-toastify";
 import { Banner } from "@/components/atoms/Banner/Banner";
-import { Button } from "@/components/atoms/Button/Button";
 import { Card } from "@/components/atoms/Card/Card";
 import { Footer } from "@/components/atoms/Footer/Footer";
 import { Header } from "@/components/atoms/Header/Header";
 import { InfoCard } from "@/components/atoms/InfoCard/InfoCard";
 import { Spinner } from "@/components/atoms/Spinner/Spinner";
-import { DownloadPDF, TableData } from "@/utils/api/airtableEndPoints";
+import { TableData } from "@/utils/api/airtableEndPoints";
 import ScoreCard, { ResponseData } from "./scoreCard";
 
 export default function Report() {
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
   const [responseData, setResponseData] = useState<ResponseData[]>();
   const [companyData, setCompanyData] = useState<ResponseData>();
 
   const { company } = router.query;
   const BACKEND_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
+
   useEffect(() => {
     if (company) {
       TableData(company[0])
@@ -42,29 +40,6 @@ export default function Report() {
     }
   }, [responseData]);
 
-  const handleDownloadPdf = () => {
-    setIsLoading(true);
-    if (company)
-      DownloadPDF(company[0])
-        .then((response) => {
-          const blob = new Blob([response.data], { type: "application/pdf" });
-          const url = window.URL.createObjectURL(blob);
-          const a = document.createElement("a");
-          a.href = url;
-          a.download = "seo-audit-report.pdf";
-          // a.click();
-          window.URL.revokeObjectURL(url);
-          toast.success("PDF downloaded successfully");
-        })
-        .catch((error) => {
-          // eslint-disable-next-line no-console
-          console.error("Error downloading PDF:", error);
-        })
-        .finally(() => {
-          setIsLoading(false);
-        });
-  };
-
   return (
     <>
       <Header companyLogo="/img/LeadwalnutIcon.svg" altText="Company logo" />
@@ -82,16 +57,12 @@ export default function Report() {
                 companyData={companyData}
               />
             </Card>
-            <a href={`${BACKEND_BASE_URL}/generate-pdf?queryParam1=${company}`}>
-            <Button
-              className="my-3 mx-2.5 md:my-5 md:mx-0"
-              onClick={handleDownloadPdf}
-              trailingIcon="/img/download-icon.svg"
-              disabled={isLoading}
-            >
-              {isLoading ? <Spinner /> : "Download Report in PDF"}
-            </Button>
+            {
+              company && <a href={`${BACKEND_BASE_URL}/generate-pdf?companyName=${company}`} target="_blank" className="flex font-semibold justify-center bg-[#78C317] text-white py-2 px-5 rounded-xl hover:bg-[#5A960C] w-fit mt-5" rel="noreferrer">
+              Download Report in PDF <img src="/img/download-icon.svg" alt="download icon" className="ml-3" />
             </a>
+            }
+            
           </>
         ) : (
           <Spinner />
