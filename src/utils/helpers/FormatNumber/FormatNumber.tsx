71f7eb0af1bd43be93121c19c5b54ef02166e616
Lovepreet <lovepreet@codewalnut.com>
2023-10-12 00:51:06 +0530

    Add: helper function for format numbers

---
+++ src/utils/helpers/FormatNumber/FormatNumber.tsx
@@ -0,0 +1,10 @@
+export function FormatNumber(number: number | string) {
+    const value = Number(number);
+    if (value && value >= 1000) {
+        const formattedNumber = (value / 1000).toFixed(1);
+        return formattedNumber.endsWith(".0")
+          ? `${formattedNumber.slice(0, -2)}k`
+          : `${formattedNumber}k`;
+      }
+    return number;
+  }
